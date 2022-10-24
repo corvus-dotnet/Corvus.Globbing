9595eeba7ad5b136a899fd37947b0f6935baaf01
Matthew Adams <matthew.adams@endjin.com>
2022-10-24 14:17:13 +0100

    Updated benchmarks.

--- Solutions/Corvus.Globbing.Benchmarks/Program.cs
+++ Solutions/Corvus.Globbing.Benchmarks/Program.cs
@@ -17,10 +17,10 @@ namespace Corvus.Globbing.Benchmarks
         /// <param name="args">Program arguments.</param>
         public static void Main(string[] args)
         {
-            ////BenchmarkRunner.Run<BaselineRegexGlobCompileBenchmarks>();
-            ////BenchmarkRunner.Run<BaselineRegexCompileAndMatchTrueBenchmarks>();
-            ////BenchmarkRunner.Run<BaselineRegexCompileAndMatchFalseBenchmarks>();
-            ////BenchmarkRunner.Run<BaselineRegexIsMatchTrueBenchmarks>`();
+            BenchmarkRunner.Run<BaselineRegexGlobCompileBenchmarks>();
+            BenchmarkRunner.Run<BaselineRegexCompileAndMatchTrueBenchmarks>();
+            BenchmarkRunner.Run<BaselineRegexCompileAndMatchFalseBenchmarks>();
+            BenchmarkRunner.Run<BaselineRegexIsMatchTrueBenchmarks>();
             BenchmarkRunner.Run<BaselineRegexIsMatchFalseBenchmarks>();
         }
     }
